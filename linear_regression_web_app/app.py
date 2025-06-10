4c0210e973afef2c8f94cd8ff11cc046dc6ce855
Joy <97067787+joyn3698@users.noreply.github.com>
2025-06-10 19:15:43 -0400

    Update app.py

--- linear_regression_web_app/app.py
+++ linear_regression_web_app/app.py
@@ -41,11 +41,11 @@ with left_col :
     <div style='border-right: 2px solid #dee2e6; padding-right: 20px; height: 100%;'>
         <h4>🤖 Evaluation</h4>
         <ul>
-            <li>R² = 0.9809</li>
-            <li>Adjusted R² = 0.9803</li>
-            <li>MAE = 8.43</li>
-            <li>MSE = 103.92</li>
-            <li>RMSE = 10.19</li>
+            <li>R² = 0.982</li>
+            <li>Adjusted R² = 0.981</li>
+            <li>MAE = 8.09</li>
+            <li>MSE = 101.91</li>
+            <li>RMSE = 10.09</li>
             <li>Residuals are nearly normal</li>
         </ul>
     </div>
@@ -54,14 +54,13 @@ with left_col :
 with right_col:
     x1 = st.number_input("Enter the Avg session length with the stylist(30-60 minutes)", min_value=30, max_value=60, value=None)
     x2 = st.number_input("Enter the time spent on the app(minutes)", min_value=0, max_value=100, value=None)
-    x3 = st.number_input("Enter the time spent on the website(minutes)", min_value=0, max_value=100, value=None)
-    x4 = st.number_input("Length of membership in months(0-12)", min_value=0, max_value=12, value=None)
+    x3 = st.number_input("Length of membership in months(0-12)", min_value=0, max_value=12, value=None)
     
     col1, col2 = st.columns([1, 2])
     
     with col1:
         if st.button("OK"):
-            user_input = [[x1, x2, x3, x4]]
+            user_input = [[x1, x2, x3]]
             pred = model.predict(user_input)
             st.session_state['predicted'] = int(round(pred[0], 0))
     
